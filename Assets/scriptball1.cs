279cbf565644d373e5c5964e40b44ff8e00b775e
rizkinasafira22 <62419208+rizkinasafira22@users.noreply.github.com>
2020-04-03 15:16:48 +0700

    nambah anim

--- Assets/scriptball1.cs
+++ Assets/scriptball1.cs
@@ -5,31 +5,40 @@ using UnityEngine;
 public class scriptball1 : MonoBehaviour
 {
     public int speed = 30;
-    public Rigidbody2D sebuah;
     // Start is called before the first frame update
+    public Rigidbody2D sesuatu;
+
+    public Animation anim;
+
+    public Animator animtr;
     void Start()
     {
-        sebuah.velocity = new Vector2(-1, -1) * speed;
+        sesuatu.velocity = new Vector2 (-1,-1)*speed;
+        animtr.SetBool("IsMove",true); 
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if(sesuatu.velocity.x > 0){ // bola bergerak ke kanan
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(1, 1, 1);
+        }else{
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(-1, 1, 1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other) {
-        if(other.collider.name=="DindingKanan" || other.collider.name=="DindingKiri"){
+        if(other.collider.name=="DindingKanan"||other.collider.name=="DindingKiri"){
             StartCoroutine(jeda());
-        }
+        }    
     }
+    
     IEnumerator jeda(){
-        sebuah.velocity = new Vector2(0,0)*speed;
-        GetComponent<Transform>().position = new Vector2(8,0);
+        sesuatu.velocity = Vector2.zero;
+        animtr.SetBool("IsMove", false);
+        sesuatu.GetComponent<Transform>().position = Vector2.zero;
         yield return new WaitForSeconds(1);
-        sebuah.velocity = new Vector2(-1, -1) * speed;
-
-        
-        
-}
+        sesuatu.velocity = new Vector2 (-1,-1)*speed;
+         animtr.SetBool("IsMove", true);
+    }
 }
