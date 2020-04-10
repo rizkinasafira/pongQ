ac4ad852a0601abcd449fc8aea0cee97e89d8b74
rizkinasafira22 <62419208+rizkinasafira22@users.noreply.github.com>
2020-04-10 19:42:30 +0700

    nambah beranda

--- Assets/TimerScript.cs
+++ Assets/TimerScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class TimerScript : MonoBehaviour
 {
     public int counter = 30;
@@ -28,6 +28,6 @@ public class TimerScript : MonoBehaviour
         timerText.text = counter.ToString();
         }
 
-        timerText.text = "GAME OVER";
+        SceneManager.LoadScene("gameover");
     }
 }
